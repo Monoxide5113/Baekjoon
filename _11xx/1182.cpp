e682fc0c7d2cb476d59e5513330e256c7915861a
Monoxide5113 <147777900+Monoxide5113@users.noreply.github.com>
2023-12-25 13:11:03 +0900

    Update

--- _11xx/1182.cpp
+++ _11xx/1182.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 const int MAX = 20;
-int N, S, seq[MAX], res;
+int N, S, seq[MAX], res = 0, curSum = 0;
 
 void Dfs(int cur, int curSum){
     if(cur >= N) return;
     
     if(curSum + seq[cur] == S) res++;
+    curSum += seq[cur];
     Dfs(cur+1, curSum);
-    Dfs(cur+1, curSum + seq[cur]);
+    curSum -= seq[cur];
+    Dfs(cur+1, curSum);
 }
 
 int main(){

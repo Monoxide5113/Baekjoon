8990a3eb19a04e673b32441cfb7f4577534c12ca
Monoxide5113 <monoxide5113@gmail.com>
2025-01-31 15:30:58 +0900

    Refactor: Refactor Code Logic

--- _1xxx/_17xx/1700(sol1).cpp
+++ _1xxx/_17xx/1700(sol1).cpp
@@ -15,14 +15,14 @@ int main()
     std::vector<int> schedule(k);
     for (auto& val : schedule)
         std::cin >> val;
-    
-    std::vector<int> multitap(n, EMPTY_SLOT);
+
+    std::vector<int> extension_cord(n, EMPTY_SLOT);
 
     auto find_replace_idx = [&](int start_idx) {
         int replace_idx, furthest_idx = 0;
         for (int i = 0; i < n; ++i) {
             const auto start_it = schedule.begin() + start_idx + 1;
-            const auto it = std::find(start_it, schedule.end(), multitap[i]);
+            const auto it = std::find(start_it, schedule.end(), extension_cord[i]);
             if (it == schedule.end()) return i;
 
             const int nxt_used_idx = std::distance(schedule.begin(), it);
@@ -35,17 +35,17 @@ int main()
 
     int res = 0;
     for (int i = 0; i < k; ++i) {
-        const auto cur_it = std::find(multitap.begin(), multitap.end(), schedule[i]);
-        if (cur_it != multitap.end()) continue;
+        const auto cur_it = std::find(extension_cord.begin(), extension_cord.end(), schedule[i]);
+        if (cur_it != extension_cord.end()) continue;
 
-        const auto empty_it = std::find(multitap.begin(), multitap.end(), EMPTY_SLOT);
-        if (empty_it != multitap.end()) {
+        const auto empty_it = std::find(extension_cord.begin(), extension_cord.end(), EMPTY_SLOT);
+        if (empty_it != extension_cord.end()) {
             *empty_it = schedule[i];
             continue;
         }
 
         const int replace_idx = find_replace_idx(i);
-        multitap[replace_idx] = schedule[i];
+        extension_cord[replace_idx] = schedule[i];
         ++res;
     }
 
